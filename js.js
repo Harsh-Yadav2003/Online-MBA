e98485cbb892534154a9728661b78a55b4a0b5b0
unknown <you@example.com>
2024-11-29 16:30:56 +0530

    initial change

--- js.js
+++ js.js
@@ -8,3 +8,14 @@ accordionItems.forEach((item) => {
     content.classList.toggle('active');
   });
 });
+
+document.querySelectorAll('.accordion-button').forEach(button => {
+  button.addEventListener('click', function () {
+    const collapseElement = this.nextElementSibling; // Find the collapse element
+    if (collapseElement.classList.contains('collapse')) {
+      collapseElement.classList.remove('collapse'); // Remove the 'collapse' class to show
+    } else {
+      collapseElement.classList.add('collapse'); // Add 'collapse' class to hide
+    }
+  });
+});
